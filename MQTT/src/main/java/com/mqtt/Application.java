85a6091ae5af42fbdd2e4e8556a0143311868411
dxz <1205760997@qq.com>
2022-08-09 16:23:05 +0800

    MQTT

---
+++ MQTT/src/main/java/com/mqtt/Application.java
@@ -0,0 +1,16 @@
+package com.mqtt;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 来源:https://blog.csdn.net/qq_37949192/article/details/103962848
+@MapperScan(basePackages = {"com.mqtt.mapper"})
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
